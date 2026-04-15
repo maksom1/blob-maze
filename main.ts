d22247b7b80b4bab3d4b9b683f530d63c78304e4
maksom1 <mightymaks18@gmail.com>
2026-04-15 14:56:24 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,38 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.jewels.jewel1, function (sprite, location) {
+    for (let index = 0; index < 10000; index++) {
+        tiles.setCurrentTilemap(tilemap`level2`)
+        mySprite.setPosition(13, 250)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.skillmap.islandTile4, function (sprite, location) {
+    for (let index = 0; index < 10000; index++) {
+        mySprite.setPosition(13, 250)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.jewels.jewel3, function (sprite, location) {
+    mySprite.setPosition(13, 250)
+    tiles.setCurrentTilemap(tilemap`level3`)
+})
+let mySprite: Sprite = null
+tiles.setCurrentTilemap(tilemap`level1`)
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 6 6 6 6 6 . . . . . 
+    . . . . . . 6 f 6 f 6 . . . . . 
+    . . . . . . 6 6 6 6 6 . . . . . 
+    . . . . . . 6 f f f 6 . . . . . 
+    . . . . . . 6 6 6 6 6 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.player1.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
+mySprite.setPosition(13, 250)
